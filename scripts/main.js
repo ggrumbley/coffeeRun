1ed1dc8536030308c8832cb3531e0d3a8d4ba475
gary <ggrumbley@ucsd.edu>
2016-11-22 11:11:17 -0800

    Markup and bootstrap added

--- scripts/main.js
+++ scripts/main.js
@@ -5,5 +5,12 @@
   var DataStore = App.DataStore;
   var myTruck = new Truck('ncc-1701', new DataStore());
   var sciTruck = new Truck('USCSS Nostromo', new DataStore());
+  myTruck.createOrder({ emailAddress: 'gary@aol.com', coffee: 'latte'});
+  myTruck.createOrder({ emailAddress: 'poo@aohell.com', coffee: 'Hammerhead'});
+  myTruck.createOrder({ emailAddress: 'fritz@aol.com', coffee: 'Americano'});
+  sciTruck.createOrder({ emailAddress: 'picard@aol.com', coffee: 'Earl Gray, HOT'});
+  sciTruck.createOrder({ emailAddress: 'ripley@aol.com', coffee: 'Acid spit'});
   window.myTruck = myTruck;
+  window.sciTruck = sciTruck;
+
 })(window);
