1abb6f94a90f83fea9b2dfa6c92c2259a042ed76
gary <ggrumbley@ucsd.edu>
2016-11-28 16:07:03 -0800

    Coffee add and delete order feature added

--- scripts/checklist.js
+++ scripts/checklist.js
@@ -15,8 +15,56 @@
     }
   }
 
+  CheckList.prototype.addClickHandler = function (fn) {
+    this.$element.on('click', 'input', function (event) {
+      var email = event.target.value;
+      this.removeRow(email);
+      fn(email);
+    }.bind(this));
+  };
+
+  CheckList.prototype.addRow = function (coffeeOrder) {
+    // Remove duplicate order (one order per person)
+    this.removeRow(coffeeOrder.emailAddress);
+
+    var rowElement = new Row(coffeeOrder);
+
+    this.$element.append(rowElement.$element);
+  }
+
+  CheckList.prototype.removeRow = function (email) {
+    this.$element
+      .find('[value="' + email + '"]')
+      .closest('[data-coffee-order="checkbox"]')
+      .remove();
+  }
+
   function Row(coffeeOrder) {
-    //TODO
+    var $div = $('<div></div>', {
+      'data-coffee-order': 'checkbox',
+      'class': 'checkbox'
+    });
+
+    var $label = $('<label></label>');
+
+    var $checkbox = $('<input></input>', {
+      type: 'checkbox',
+      value: coffeeOrder.emailAddress
+    });
+
+    var description = coffeeOrder.size + ' ';
+    if (coffeeOrder.flavor) {
+      description += coffeeOrder.flavor + ' ';
+    }
+    description += coffeeOrder.coffee + ', ';
+    description += ' (' + coffeeOrder.emailAddress + ')';
+    description += ' [' + coffeeOrder.strength + 'x]';
+
+    $label.append($checkbox);
+    $label.append(description);
+    $div.append($label);
+
+    this.$element = $div;
   }
   App.CheckList = CheckList;
   window.App = App;
--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,25 @@
 (function(window) {
   'use strict';
   var FORM_SELECTOR = '[data-coffee-order="form"]';
+  var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]'
 
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
-
   var FormHandler = App.FormHandler;
+  var CheckList = App.CheckList;
+
   var myTruck = new Truck('ncc-1701', new DataStore());
-  var sciTruck = new Truck('USCSS Nostromo', new DataStore());
-
-  myTruck.createOrder({ emailAddress: 'gary@aol.com', coffee: 'latte'});
-  myTruck.createOrder({ emailAddress: 'poo@aohell.com', coffee: 'Hammerhead'});
-  myTruck.createOrder({ emailAddress: 'fritz@aol.com', coffee: 'Americano'});
-  sciTruck.createOrder({ emailAddress: 'picard@aol.com', coffee: 'Earl Gray, HOT'});
-  sciTruck.createOrder({ emailAddress: 'ripley@aol.com', coffee: 'Acid spit'});
-  
   window.myTruck = myTruck;
-  window.sciTruck = sciTruck;
 
+  var checkList = new CheckList(CHECKLIST_SELECTOR);
+  checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
+  
   var formHandler = new FormHandler(FORM_SELECTOR);
-  formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
-  console.log(formHandler);
+
+  formHandler.addSubmitHandler(function (data) {
+    myTruck.createOrder.call(myTruck, data);
+    checkList.addRow.call(checkList, data);
+  });
+
 })(window);
